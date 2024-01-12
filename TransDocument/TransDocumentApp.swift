10a752423797040b0f341e5be040869fb69c2400
FreeDeveloper97 <freedeveloper97@gmail.com>
2024-01-12 09:57:34 +0900

    Feat: 프로젝트 생성

---
+++ TransDocument/TransDocumentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransDocumentApp.swift
+//  TransDocument
+//
+//  Created by Kang Minsang on 2024/01/12.
+//
+
+import SwiftUI
+
+@main
+struct TransDocumentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
